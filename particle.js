5fa94a0de4c9a5ae504d29e3e391bf981c5cdef5
ricsyk <richiesykes@protonmail.com>
2022-03-21 15:06:48 +0000

    Finished tutorial

---
+++ particle.js
@@ -0,0 +1,46 @@
+const FRICTION = 0.98;
+const COLOR_SPEED = 0.22;
+const MOVE_SPEED = 0.95;
+
+export class Particle {
+    constructor (pos, texture) {
+        this.sprite = new PIXI.Sprite(texture);
+        this.sprite.scale.set(0.06);
+
+        this.savedX = pos.x;
+        this.savedY = pos.y;
+
+        this.x = pos.x;
+        this.y = pos.y;
+
+        this.sprite.x = this.x;
+        this.sprite.y = this.y;
+
+        this.vx = 0;
+        this.yx = 0;
+        this.radius = 10;
+
+        this.savedRgb = 0xf3316e;
+        this.rgb = 0xf3316e;
+    }
+
+    collide() {
+        this.rgb = 0x451966;
+    }
+
+    draw() {
+        this.rgb += (this.savedRgb - this.rgb) * COLOR_SPEED;
+        this.x += (this.savedX - this.x) * MOVE_SPEED;
+        this.y += (this.savedY - this.y) * MOVE_SPEED;
+
+        this.vx *= FRICTION;
+        this.yx *= FRICTION;
+
+        this.x += this.vx;
+        this.y += this.yx;
+        
+        this.sprite.x = this.x;
+        this.sprite.y = this.y;
+        this.sprite.tint = this.rgb;
+    }
+}
